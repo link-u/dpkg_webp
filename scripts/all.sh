578e033e4805a4d9e193b8e9456a3eaac31246f2
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-06-10 05:00:26 +0900

    update: tiff のビルド処理を all.sh → prepare_build.sh に移動.

--- scripts/all.sh
+++ scripts/all.sh
@@ -14,13 +14,7 @@ env --chdir="${scripts_dir}/../${pkg_dir}" \
   --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' \
   debian/control
 
-env --chdir="${scripts_dir}/../tiff" rm -rf tiff-4.1.0 || true;
-env --chdir="${scripts_dir}/../tiff" tar xvf tiff-4.1.0.tar.gz
-env --chdir="${scripts_dir}/../tiff/tiff-4.1.0" ./configure --prefix=/opt/tiff --disable-webp --enable-shared=no
-env --chdir="${scripts_dir}/../tiff/tiff-4.1.0" make clean -j"$(nproc)"
-env --chdir="${scripts_dir}/../tiff/tiff-4.1.0" make -j"$(nproc)"
-env --chdir="${scripts_dir}/../tiff/tiff-4.1.0" make install
-
 ## deb ファイルのビルド
+bash "${scripts_dir}/prepare_build.sh"
 bash "${scripts_dir}/create_changelog.sh"
 bash "${scripts_dir}/build.sh"
---
+++ scripts/prepare_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+## git リポジトリ上の scripts ディレクトリのパスを取得
+scripts_dir=$(readlink -f $(cd $(dirname $0) && pwd))
+
+## tiff ソースの tar ball 名を取得
+src_tiff="$(basename $(ls -1vd ${scripts_dir}/../tiff/tiff-*.tar.gz | tail -n 1) .tar.gz)"
+echo $src_tiff
+
+## tiff のビルド
+#  * 共有ライブラリはつくらない. 静的ライブラリのみ生成
+env --chdir="${scripts_dir}/../tiff" rm -rf "${src_tiff}" || true;
+env --chdir="${scripts_dir}/../tiff" tar xvf "${src_tiff}.tar.gz"
+env --chdir="${scripts_dir}/../tiff/${src_tiff}" ./configure --prefix=/opt/tiff --disable-webp --enable-shared=no
+env --chdir="${scripts_dir}/../tiff/${src_tiff}" make clean -j"$(nproc)"
+env --chdir="${scripts_dir}/../tiff/${src_tiff}" make -j"$(nproc)"
+env --chdir="${scripts_dir}/../tiff/${src_tiff}" make install
