c4fd8adbcf68fe76efc0cd613b9fb2ccc2aadbc9
psi <ryo.hirafuji@link-u.co.jp>
2021-07-02 17:33:49 +0900

    Fix script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,20 +3,20 @@
 set -eu
 
 ## git リポジトリ上の root のパスを取得
-root_dir="$(cd "$(dirname "$(readlink -f "$0")")" & cd .. && pwd)"
+root_dir="$(cd "$(dirname "$(readlink -f "$0")")" && cd .. && pwd)"
 
 ## パッケージのディレクトリ名
 pkg_dir="webp"
 
 ## パッケージのソース tar ball のファイル名とバージョン名の取得
-src="$(basename "$(ls -1vd ${root_dir}/${pkg_dir}/*.tar.gz | tail -n 1)")"
-version="$(basename "${src}" .tar.gz)"
+src="$(basename "$(find "${root_dir}/${pkg_dir}" -type f -name "*.tar.gz")")"
+libname="$(basename "${src}" ".tar.gz")"
 
 ## パッケージのソース tar ball の展開
 env --chdir="${root_dir}/${pkg_dir}/" tar xvf "${src}"
 
 ## 展開したパッケージのソースのディレクトリ名取得
-src_dir="$(basename "$(ls -1vd ${root_dir}/${pkg_dir}/*${version} | tail -n 1)")"
+src_dir="$(basename "${root_dir}/${pkg_dir}/"*"${libname}")"
 
 ## debian package のビルド
 env --chdir="${root_dir}/${pkg_dir}/" cp -r debian "${src_dir}";
